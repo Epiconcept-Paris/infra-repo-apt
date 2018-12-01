af0cd3fac63cdf26df83a4522de7ff565f308b16
Christophe de Traversay <epiconcept@traversay.com>
2018-12-01 02:14:26 +0100

    Update site/gpg/ to create a signing subkey and export it as key.gpg Enhance genkey.sh to change to its directory if needed

--- site/gpg/genkey.sh
+++ site/gpg/genkey.sh
@@ -3,16 +3,25 @@
 #	genkey.sh - Generate a GPG key for APT signing
 #
 Prg=`basename $0`
-test -f key.conf || { echo "$Prg: cannot find ./key.conf (must cd to our dir)" >&2; exit 1; }
-Mail="`sed -n 's/^Name-Email: //p' key.conf`"
-if gpg -k | grep "^uid .* <$Mail>$" >/dev/null; then
-    echo "Deleting existing key found for $Mail:"
+Cnf=key.conf
+#   Paths are relative, so move to our top directory
+if ! [ -f $Cnf ]; then
+    cd `dirname $0`
+    if ! [ -f $Cnf ]; then
+	echo "$Prg: cannot find '$Cnf' configuration file" >&2
+	exit 2
+    fi
+fi
+Name="`sed -n 's/^Name-Real: //p' $Cnf`"
+Mail="`sed -n 's/^Name-Email: //p' $Cnf`"
+if gpg -k | grep "^uid  *$Name <$Mail>$" >/dev/null; then
+    echo "Deleting existing keys found for $Mail:"
     gpg -k $Mail | sed 's/^/    /'
     fp=`gpg --fingerprint $Mail | sed -n 's/^ *Key fingerprint = //p' | tr -d ' '`
-    #fp=`gpg -K $Mail | sed -nr 's;^sec +([^ ]+) .*$;\1;p'`
     gpg --batch --delete-secret-and-public-key $fp
 fi
 echo "Please generate entropy with 'sudo grep -Lr SomeImpossibleString /'..."
-gpg --gen-key --batch key.conf
-gpg -k $Mail
-gpg --armor --export $Mail >key.gpg
+gpg --gen-key --batch $Cnf
+gpg -k
+Sign=`gpg -k --with-colons $Mail | awk -F: '$1 == "sub" {print substr($5,9)}'`
+gpg -a --export $Sign >key.gpg
