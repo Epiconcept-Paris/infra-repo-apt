e5ac9290ddf86db7e022273fc569ca8aeff477f4
Christophe de Traversay <epiconcept@traversay.com>
2018-12-02 08:50:31 +0100

    Add some comments to genkey.sh

--- site/gpg/genkey.sh
+++ site/gpg/genkey.sh
@@ -4,6 +4,7 @@
 #
 Prg=`basename $0`
 Cnf=key.conf
+
 #   Paths are relative, so move to our top directory
 if ! [ -f $Cnf ]; then
     cd `dirname $0`
@@ -14,14 +15,22 @@ if ! [ -f $Cnf ]; then
 fi
 Name="`sed -n 's/^Name-Real: //p' $Cnf`"
 Mail="`sed -n 's/^Name-Email: //p' $Cnf`"
+
+#   Delete any existing key
 if gpg -k | grep "^uid  *$Name <$Mail>$" >/dev/null; then
     echo "Deleting existing keys found for $Mail:"
     gpg -k $Mail | sed 's/^/    /'
     fp=`gpg --fingerprint $Mail | sed -n 's/^ *Key fingerprint = //p' | tr -d ' '`
     gpg --batch --delete-secret-and-public-key $fp
 fi
+
+#   Generate new keys
 echo "Please generate entropy with 'sudo grep -Lr SomeImpossibleString /'..."
 gpg --gen-key --batch $Cnf
 gpg -k
+
+#   Export the public signing subkey to be copied in the repo
+#   APT requires the Release files to be signed with a *certified* key and
+#	only the subkey (certified by the main key) fulfils that requirement
 Sign=`gpg -k --with-colons $Mail | awk -F: '$1 == "sub" {print substr($5,9)}'`
 gpg -a --export $Sign >key.gpg
